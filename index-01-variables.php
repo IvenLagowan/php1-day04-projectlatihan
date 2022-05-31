fd30db359e6f0f531d8f302c37235579cc5e5496
Iven DF <94290469+ivendf@users.noreply.github.com>
2022-05-30 21:00:20 -0700

    finis coder

---
+++ index-01-variables.php
@@ -0,0 +1,12 @@
+<?php
+
+  //  Tag variables
+  $title = "EromaCoder";
+  $tagLine = 'Learn To Coder';
+
+
+  // Data siswa aktif
+  $siswa1 = 'Iven Lagowan';
+  $siswa2 = 'Age Matuan';
+  $siswa3 = 'Jansen Meage';
+ ?>
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<?php   include('index-01-variables.php'); ?>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title><?php echo $title; ?></title>
+  </head>
+  <body>
+    <h1><?php echo $tagLine; ?></h1>
+  </body>
+</html>
